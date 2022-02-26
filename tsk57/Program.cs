c63fc4214395c0a0b10549dc32e27471df2e500c
Artem <40538552+surge603@users.noreply.github.com>
2022-02-26 22:05:48 +0300

    Задача 57

---
+++ tsk57/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 57: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int y = int.Parse(Console.ReadLine());
+int[,] arr = new int[x, y];
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(10, 100);
+        }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, k] > arr[i, j])
+                {
+                    int max = arr[i, k];
+                    arr[i, k] = arr[i, j];
+                    arr[i, j] = max;
+                }
+            }
+
+        }
+    }
+}
+
+FillArray();
+Console.WriteLine();
+Console.WriteLine("Исходный массив:");
+PrintArray();
+SortArray();
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
+PrintArray();
+
